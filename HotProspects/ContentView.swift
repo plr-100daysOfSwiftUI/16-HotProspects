617d5c25b7e02e63cd7f695f05a4edfd823a3a97
Paul Richardson <developer@photographerafoot.com>
2020-10-20 09:15:10 +0200

    Initial Commit

---
+++ HotProspects/ContentView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by Paul Richardson on 14.10.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+	
+	var prospects = Prospects()
+	
+	var body: some View {
+		TabView {
+			
+			ProspectsView(filter: .none)
+				.tabItem {
+					Image(systemName: "person.3")
+					Text("Everyone")
+				}
+
+			ProspectsView(filter: .contacted)
+				.tabItem {
+					Image(systemName: "checkmark.circle")
+					Text("Contacted")
+				}
+
+			ProspectsView(filter: .uncontacted)
+				.tabItem {
+					Image(systemName: "questionmark.diamond")
+					Text("Uncontacted")
+				}
+			
+			MeView()
+				.tabItem {
+					Image(systemName: "person.crop.square")
+					Text("Me")
+				}
+			
+		}
+		.environmentObject(prospects)
+	}
+	
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+	static var previews: some View {
+		ContentView()
+	}
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# HotProspects
+
+[Hacking with Swift: 100 Days of SwiftUI - Project 16][1]
+
+### Challenges
+
+1. Add an icon to the "Everyone" screen showing whether a prospect was contacted or not.
+2. Use JSON and the documents directory for saving and loading our user data.
+3. Use an action sheet to customize the way users are sorted in each screen – by name or by most recent.
+
+[1]: https://www.hackingwithswift.com/100/swiftui/85
+
+
